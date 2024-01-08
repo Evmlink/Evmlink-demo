f3894b2803863d6fbdd9c0349c6fed2edc291041
Your Name <you@example.com>
2024-01-08 08:39:25 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Aptos-web3auth-aptoslink-demo
+
+This repo is fork from [Web3authSOLANA](https://github.com/aptos-labs/aptos-wallet-adapter/tree/main) and combine from [Aptos-Wallet-Adapter-React](https://github.com/aptos-labs/aptos-wallet-adapter/tree/main/packages/wallet-adapter-react#examples)
+
+## Important Links
+
+- [Twitter](https://twitter.com/aptoslinker)
+- [Docs](https://evmlinks.gitbook.io/aptoslink/)
+- [Dorahacks](https://dorahacks.io/buidl/8591)
---
+++ src/App.tsx
@@ -0,0 +1,206 @@
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, IProvider } from "@web3auth/base";
+import RPC from "./solanaRPC";
+import "./App.css";
+
+// Plugins
+import { SolanaWalletConnectorPlugin } from "@web3auth/solana-wallet-connector-plugin";
+
+// Adapters
+import { SolflareAdapter } from "@web3auth/solflare-adapter";
+import { SlopeAdapter } from "@web3auth/slope-adapter";
+
+const clientId = "BCAAiDZXdCWOyncD7Dgtazac1_0C6jQZFxiSKxA-wSv3FW6iFpGi68PW7L5XyE1hRoeeRS3hSh_-rZOg_Ou4eSk"; // get from https://dashboard.web3auth.io
+
+function App() {
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.SOLANA,
+            chainId: "0x3", // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
+            rpcTarget: "https://summer-frosty-friday.solana-devnet.quiknode.pro/5430f85cfb9a90ac2763131b24d8a746f2d18825/", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+          // uiConfig refers to the whitelabeling options, which is available only on Growth Plan and above
+          // Please remove this parameter if you're on the Base Plan
+          uiConfig: {
+            appName: "Aptos test",
+            mode: "light",
+            // loginMethodsOrder: ["apple", "google", "twitter"],
+            logoLight: "https://web3auth.io/images/web3auth-logo.svg",
+            logoDark: "https://web3auth.io/images/web3auth-logo---Dark.svg",
+            defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+            loginGridCol: 3,
+            primaryButton: "externalLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+          },
+          web3AuthNetwork: "sapphire_devnet",
+        });
+
+        // adding solana wallet connector plugin
+
+        const torusPlugin = new SolanaWalletConnectorPlugin({
+          torusWalletOpts: {},
+          walletInitOptions: {
+            whiteLabel: {
+              name: "Whitelabel Demo",
+              theme: { isDark: true, colors: { torusBrand1: "#00a8ff" } },
+              logoDark: "https://web3auth.io/images/web3auth-logo.svg",
+              logoLight: "https://web3auth.io/images/web3auth-logo---Dark.svg",
+              topupHide: true,
+              defaultLanguage: "en",
+            },
+            enableLogging: true,
+          },
+        });
+        await web3auth.addPlugin(torusPlugin);
+
+        const solflareAdapter = new SolflareAdapter({
+          clientId,
+        });
+        web3auth.configureAdapter(solflareAdapter);
+
+        const slopeAdapter = new SlopeAdapter({
+          clientId,
+        });
+        web3auth.configureAdapter(slopeAdapter);
+
+        setWeb3auth(web3auth);
+
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
+
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connect();
+
+    if (web3auth.connected) {
+      setLoggedIn(true);
+      console.log("🔥 Connected")
+      if (!provider) {
+        uiConsole("provider not initialized yet");
+        return;
+      }
+      const rpc = new RPC(provider);
+      const privateKey = await rpc.getPrivateKey();
+      console.log("🔥 Connected : ",privateKey)
+    }
+    setProvider(web3authProvider);
+  };
+
+  const addChain = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const newChain = {
+      chainId: "0x2",
+      displayName: "Solana Testnet",
+      chainNamespace: CHAIN_NAMESPACES.SOLANA,
+      tickerName: "SOLANA",
+      ticker: "SOL",
+      decimals: 18,
+      rpcTarget: "https://api.testnet.solana.com",
+      blockExplorer: "https://explorer.solana.com/?cluster=testnet",
+    };
+    await web3auth?.addChain(newChain);
+    uiConsole("New Chain Added");
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+    setLoggedIn(false);
+  };
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="https://github.com/Evmlink/Aptoslink-npm" rel="noreferrer">
+          Web3Auth{" "}
+        </a>
+        & ReactJS For Aptos
+      </h1>
+
+      <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
+
+      <footer className="footer">
+        <a
+          href="https://github.com/Evmlink/Aptoslink-npm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
